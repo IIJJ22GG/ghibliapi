d30453f53e78863ba3cd2a7e7d35b96ef7ab7331
IIJJ22GG <72188231+IIJJ22GG@users.noreply.github.com>
2022-12-14 19:26:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const page = require('./public/index.html')
 const data = require('./data.json')
 addEventListener('fetch', event => {
-  const json = JSON.stringify(data, null, 2);
-
-  return event.respondWith(
-    new Response(json, {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8'
-      }
-    })
-  )
+    url = location.pathname
+    switch(url){case '': case false:
+        return event.respondWith( new Response(page, {
+            headers: { 'content-type': 'application/html' }
+        }))
+    }
+    let response = data
+    url = url.split('/')
+    l:for (let i=0, n=url.length; i < n; ++i){ o=url[i]
+        switch(o){case '': continue}
+        switch(response.hasOwnProperty(o)){case true: response = response[o]; continue}
+        switch(response[0]){case undefined:break; default: 
+            for (let ii=0, nn=Object.keys(response).length; ii < nn; ++ii){
+                switch(response[ii].id===o){case true: response = response[ii]; continue l}
+        }}
+        response = 404; break
+    }
+    return event.respondWith( new Response(JSON.stringify(response), {
+        headers: { 'content-type': 'application/json;charset=UTF-8' }
+    }))
 })
