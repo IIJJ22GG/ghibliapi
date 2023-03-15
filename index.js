e20ea7606d570462d3df4068b01bfe21d4ad9af5
IIJJ22GG <72188231+IIJJ22GG@users.noreply.github.com>
2023-03-14 18:13:40 -0700

    New faster code!

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const page = require('./public/index.html');
-const data = require('./data.json');
-
+const page = require('./public/index.html')
+const data = require('./data.json')
 addEventListener('fetch', event => {
-  const path = new URL(event.request.url).pathname;
-  if (path === '/' || path === '') {
-    return event.respondWith(new Response(page.default, {headers: {'content-type': 'text/html;charset=UTF-8'}}));
-  }
-  let response = data;
+  const path = new URL(event.request.url).pathname
+  if (path === '/' || path === '') { return event.respondWith(new Response(page.default, {headers: {'content-type': 'text/html;charset=UTF-8'}})) }
+  let response = data
   for (const segment of path.split('/').filter(Boolean)) {
     if (Array.isArray(response)) {
-      const id = segment;
-      response = response.find(obj => obj.id === id);
-      if (!response) {
-        return event.respondWith(new Response(JSON.stringify({status: 404}), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}}));
-      }
+      const id = segment; response = response.find(obj => obj.id === id)
+      if (!response) { return event.respondWith(new Response(JSON.stringify({status: 404}), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}})) }
     } else {
-      response = response[segment];
-      if (!response) {
-        return event.respondWith(new Response(JSON.stringify({status: 404}), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}}));
-      }
+      response = response[segment]
+      if (!response) { return event.respondWith(new Response(JSON.stringify({status: 404}), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}})) }
     }
   }
-  return event.respondWith(new Response(JSON.stringify(response), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}}));
-});
+  return event.respondWith(new Response(JSON.stringify(response), {headers: {'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*'}}))
+})
